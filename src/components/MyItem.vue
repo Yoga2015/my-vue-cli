a2cbfea74f9085a0b754bed182ce0f053bdecddf
Yoga2015 <yoga3333@126.com>
2023-04-07 00:42:40 +0800

    创建 静态组件

---
+++ src/components/MyItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="MyItem">
+    <li>
+      <label>
+        <input type="checkbox" />
+        <span>xxxxx</span>
+      </label>
+      <button class="btn btn-danger" style="display:none">删除</button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyItem'
+}
+</script>
+
+<style scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,14 +4,8 @@ import Vue from 'vue'
 // 导入 App组件，它是所有组件的父组件
 import App from '@/App.vue'
 
-// 1、引入 插件
-import plugins from './plugins.js'
-
 // 关闭 Vue的生产环境提示
 Vue.config.productionTip = false
-
-// 2、应用（使用）插件
-Vue.use(plugins)
 
 // 创建 Vue 的实例 vm
 new Vue({
