0c170191b73ed398d52d0855f6a898f1c226f210
Yoga2015 <yoga3333@126.com>
2023-04-14 23:03:16 +0800

    新增 App组件中的 todos 数据 保存到 localStorage

---
+++ 7-src-TodoList/main.js
@@ -0,0 +1,13 @@
+// 导入 Vue
+import Vue from 'vue'
+
+// 导入 App组件，它是所有组件的父组件
+import App from '@/App.vue'
+
+// 关闭 Vue的生产环境提示
+Vue.config.productionTip = false
+
+// 创建 Vue 的实例 vm
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
