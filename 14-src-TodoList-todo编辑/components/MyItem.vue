160d5639c37f996057dc67c8070785c4bc1cb8c6
Yoga2015 <yoga3333@126.com>
2023-04-20 00:51:17 +0800

    新增 ： TodoList --> todo后期编辑

--- 14-src-TodoList-todo编辑/components/MyItem.vue
+++ 14-src-TodoList-todo编辑/components/MyItem.vue
@@ -4,20 +4,19 @@
       <label>
         <!-- v-model绑定的值，不能是props传过来的值，因为props是不可以修改的！ -->
         <!-- <input type="checkbox" v-model="todo.done" /> -->
-
         <!-- :checked="done" 是为了初始化展示列表时，显示谁勾了，谁没勾-->
         <!-- @change="handleCheck(todo.id) 是为了更新后续操作，谁取消勾选，谁又勾选了-->
         <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)" />
-
-        <span>{{ todo.title }}</span>
+        <span v-show="!todo.isEdit">{{ todo.title }}</span>
+        <input v-show="todo.isEdit" type="text" :value="todo.title" 
+        ref="iptTitle" @blur="handleBlur(todo, $event)">
       </label>
-      <button class="btn btn-danger" @click="handleTodo(todo.id)">
-        删除
-      </button>
+      <button class="btn btn-danger" @click="handleTodo(todo.id)">删除</button>
+      <button v-show="!todo.isEdit" class="btn btn-edit" 
+      @click="handleEdit(todo, $event)">编辑</button>
     </li>
   </div>
 </template>
-
 <script>
 import pubsub from 'pubsub-js'
 export default {
@@ -45,6 +44,28 @@ export default {
         // 发布者： 发布消息
         pubsub.publish('deleteTodo', id)
       }
+    },
+    // 编辑 todo    这涉及到todo.id todo.title，所以传 todo
+    handleEdit(todo) {
+      // 判断 todo数组 身上是否有 isEdit  todo.hasOwnProperty('isEdit')
+      if (todo.isEdit) {
+        todo.isEdit = true
+      } else {
+        console.log('todo身上没有 isEdit');
+        this.$set(todo, 'isEdit', true)    // 后期给todo追加属性
+      }
+      // this.$nextTick(callback) 会等组件的DOM 更新完成之后，再执行 cb 回调函数。
+      this.$nextTick(function () {
+        this.$refs.iptTitle.focus()
+      })
+    },
+
+    // 失去焦点后  从 input框 切换回  文字展示 ,否则 执行修改逻辑
+    handleBlur(todo, e) {
+      todo.isEdit = false
+      if (!todo.title.trim()) return alert('输入不能为空！')
+      // console.log('updateTodo', todo.id, e.target.value);
+      this.$bus.$emit('updateTodo', todo.id, e.target.value)
     }
   },
 }
@@ -93,33 +114,5 @@ li:hover {
 
 #MyItem li:hover button {
   display: block;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
 }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="MyTest">
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <h1 v-show="isShow">你好呀！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyTest",
+  data() {
+    return {
+      isShow: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  background-color: skyblue;
+}
+</style>
