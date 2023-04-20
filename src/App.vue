bea0265c51e5c0703da48b638e64164a8ec92ceb
Yoga2015 <yoga3333@126.com>
2023-04-20 22:45:17 +0800

    新增 配置代理

--- src/App.vue
+++ src/App.vue
@@ -1,171 +1,26 @@
 <template>
   <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <!--App组件将 一个函数 传给 MyHeader子组件-->
-        <!-- <MyHeader :addTodo="addTodo" /> -->
-        <MyHeader @addTodo="addTodo" />
-
-        <!-- 将一个数组 传给 MyList 子组件 -->
-        <MyList :todos="todos" />
-
-        <!-- <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" 
-        :clearAllTodo="clearAllTodo" /> -->
-        <MyFooter :todos="todos" @checkAllTodo="checkAllTodo" @clearAllTodo="clearAllTodo" />
-      </div>
-    </div>
+    <button @click="getStudents">获取学生信息</button>
   </div>
 </template>
 
 <script>
-import pubsub from 'pubsub-js'
-import MyHeader from './components/MyHeader.vue'
-import MyList from './components/MyList.vue'
-import MyFooter from './components/MyFooter.vue'
+import axios from 'axios'
 export default {
   name: 'MyApp',
-  components: {
-    MyHeader, MyList, MyFooter
-  },
-  data() {
-    return {
-      // todos: [
-      //   // { id: '001', title: '睡觉', done: true },
-      //   // { id: '002', title: '吃饭', done: false },
-      //   // { id: '003', title: '玩游戏', done: true },
-      // ]
-      todos: JSON.parse(localStorage.getItem('todos')) || []
-    }
-  },
+  
   methods: {
-    // 添加 一条todo   数据在哪，操作数据的方法就在哪
-    addTodo(todoObj) {
-      console.log('我是APP组件,收到了数据 ', todoObj);
-      this.todos.unshift(todoObj);
-    },
 
-    // 勾选 or 取消勾选一个todo
-    checkTodo(id) {
-      this.todos.forEach((todo) => {
-        if (todo.id === id) {
-          return todo.done = !todo.done
+    getStudents() {
+      axios.get('http://localhost:5000/students').then(
+        response => {
+          console.log('请求成功了', response);
+        }, error => {
+          console.log('请求失败了', error);
         }
-      })
-    },
-
-    // 更新一个todo
-    updateTodo(id, title) {
-      this.todos.forEach((todo) => {
-        if (todo.id === id) {
-          return todo.title = title
-        }
-      })
-    },
-
-    // 删除 一条todo
-    deleteTodo(_, id) {
-      // filter()方法 不会删除 原数组
-      this.todos = this.todos.filter((todo) => {
-        return todo.id !== id
-      })
-    },
-
-    // 全选or取消全选
-    checkAllTodo(done) {
-      this.todos.forEach((todo) => {
-        todo.done = done
-      })
-    },
-
-    // 清除 所有 已完成(已勾选) 的 todo
-    clearAllTodo() {
-      this.todos = this.todos.filter((todo) => {
-        return !todo.done
-      })
+      )
     }
-  },
-  watch: {
-    // 监视 data节点中的 todos 数组  ，newValue 指的是当前的 todos 数组
-    // 简写版 的监视器 todos 只能监视一层，数组内部 或 对象内部 发生变化后监视不到！！
-    // todos(value) {
-    //   // 当用户正常添加 一条 或 多条 todo 时，实时 保存到 localStorage
-    //   localStorage.setItem('todos', JSON.stringify(value))
-    // }
 
-    // 完整版 的监视器 todos
-    todos: {
-      deep: true,
-      handler(newValue) {
-        localStorage.setItem('todos', JSON.stringify(newValue))
-      },
-    }
-  },
-  mounted() {
-    this.$bus.$on('checkTodo', this.checkTodo)
-    this.$bus.$on('updateTodo', this.updateTodo)
-    // this.$bus.$on('deleteTodo', this.deleteTodo)
-    // 订阅者：订阅消息
-    this.pubId = pubsub.subscribe('deleteTodo', this.deleteTodo)
-  },
-
-  beforeDestroy() {
-    this.$bus.$off('checkTodo')
-    this.$bus.$off('updateTodo')
-    // this.$bus.$off('deleteTodo')
-    pubsub.unsubscribe(this.pubId)
   }
 }
 </script>
-
-<style>
-/*base*/
-body {
-  background: #fff;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-edit {
-  color: #fff;
-  background-color: rgb(105, 175, 202);
-  border: 1px solid rgb(105, 175, 202);
-  margin-right: 5px;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-</style>
