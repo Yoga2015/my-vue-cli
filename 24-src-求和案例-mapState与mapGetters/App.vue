037ca86d261558ad3cb2325efc48de2f968204c8
Yoga2015 <yoga3333@126.com>
2023-05-03 15:59:31 +0800

    新增 ：求和案例-mapState与mapGetters

---
+++ 24-src-求和案例-mapState与mapGetters/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <Count></Count>
+  </div>
+</template>
+
+<script>
+import Count from './components/Count.vue'
+export default {
+  name: 'MyApp',
+  components: { Count },
+  // mounted() {
+  //   console.log('App中', this);
+  // }
+}
+</script>
