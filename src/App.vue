ca2ce03af9291f74669ffce9099143bbdb1860ed
Yoga2015 <yoga3333@126.com>
2023-05-04 23:40:17 +0800

    新增 : 完整 或 按需引入  Element UI

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="container">
-    <Count></Count>
-    <Person></Person>
+
+    <button>原生的按钮</button>
+    <input type="text">
+    <hr>
+
+
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+    <hr>
+
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-user" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+    <hr>
+
+    <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+    </el-date-picker>
+
   </div>
 </template>
 
 <script>
-import Count from './components/Count.vue'
-import Person from './components/Person.vue'
 export default {
   name: 'MyApp',
-  components: { Count, Person },
-  // mounted() {
-  //   console.log('App中', this);
-  // }
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,23 +4,25 @@ import Vue from 'vue'
 // 2、引入 App组件，它是所有组件的父组件
 import App from '@/App.vue'
 
-// 5、引入Vuex
-// import Vuex from 'vuex'
+// 5、引入 ElementUI
+// import ElementUI from 'element-ui';
 
-// 7、引入 store
-import store from './store/index.js'
+// 6、引入 ElementUI 全部样式
+// import 'element-ui/lib/theme-chalk/index.css';
+
+// 8、按需引入 
+import { Button, DatePicker } from 'element-ui';
+
+Vue.component(Button.name, Button);     // 全局注册 Button 组件
+Vue.component(DatePicker.name, DatePicker);     // 全局注册 Button 组件
 
 // 3、关闭 Vue的生产环境提示
 Vue.config.productionTip = false
 
-// 6、使用 Vuex
-// Vue.use(Vuex)
+// 7、使用 ElementUI
+// Vue.use(ElementUI)
 
 // 4、创建 Vue 的实例 vm
 new Vue({
   render: h => h(App),
-  store,    // 使用 Vuex的 store 管理组件的状态 
-  beforeCreate() {
-    Vue.prototype.$bus = this  // 安装、创建 全局事件总线
-  }
 }).$mount('#app')
